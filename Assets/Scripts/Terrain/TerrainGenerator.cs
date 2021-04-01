844b7c6d438cf82dc00a27c8e118a5ab6844e187
Mcnuttys <isaacmcnuttys50@gmail.com>
2021-04-01 13:18:55 -0400

    Terrain Complexity
    Add some additional complexity to the terrain generation.

--- Assets/Scripts/Terrain/TerrainGenerator.cs
+++ Assets/Scripts/Terrain/TerrainGenerator.cs
@@ -14,27 +14,30 @@ public enum BlockType
 public class TerrainGenerator : MonoBehaviour
 {
     public int seed = 1234;
-    public int x, y;
+    public float sealevel = 32f;
+    public float x, y;
     public Block[] blocks;
-    public AnimationCurve heightCurve;
+    public PerlinLayer[] perlinLayers;
 
     private float offset;
     private VisualChunkManager vcm;
-
-    private System.Random blockRng;
 
     void Awake()
     {
         vcm = GetComponent<VisualChunkManager>();
 
         Random.InitState(seed);
-        blockRng = new System.Random(seed);
         offset = Random.Range(1000, 999999);
     }
 
     public float GetHeight(float x, float y)
     {
-        return heightCurve.Evaluate(Mathf.PerlinNoise((x + offset) * 0.05f, (y + offset) * 0.05f)) * 15f + 32f;
+        float pValue = 0;
+        for (int i = 0; i < perlinLayers.Length; i++)
+        {
+            pValue += perlinLayers[i].Evaluate(x, y, offset);
+        }
+        return pValue + sealevel;
     }
 
     public BlockType GetBlock(float y, float surfaceHeight)
@@ -62,4 +65,16 @@ public class Block
     public BlockType type;
     public Texture2D texture;
     public Vector2 uvOffset;
+}
+
+[System.Serializable()]
+public class PerlinLayer
+{
+    public float resolution;
+    public float scale;
+
+    public float Evaluate(float x, float y, float offset)
+    {
+        return Mathf.PerlinNoise((x + offset) * (1f / resolution), (y + offset) * (1f / resolution)) * scale;
+    }
 }
